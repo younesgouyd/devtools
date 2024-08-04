68a97410aaff36215a110d3384c1d3ce96b5968c
Younes Gouyd <younes.gouyd@gmail.com>
2024-08-04 21:17:14 +0100

    initial commit

--- main/src/main/kotlin/dev/younesgouyd/apps/devtools/main/Application.kt
+++ main/src/main/kotlin/dev/younesgouyd/apps/devtools/main/Application.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.*
